0d94518c9ded6defc7e58022b98ab57f846f304e
Carabajal Nahuel <833-nacarabajal@users.noreply.gitlab.gruposancorseguros.com>
2020-10-20 14:21:39 -0300

    Se actualizo la version V2

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function AgregarTarea(event) {
 
   // Si la tarea es valida se agrega a la lista de tareas, sino, la tarea no se agrega y se muestra una advertencia
   if (!NoEsValido(taskText)) {
-    // Recorro las filas de la tabla, si la proxima esta vacia, inserto las celdas con sus elementos, guardo las tareas y vacio el input de la tarea.
+    // Recorro las filas de la tabla, si la proxima esta vacia, inserto las celdas con sus elementos y vacio el input de la tarea.
     for (var i = 0; i <= taskTable.rows.length; i++) {
       if (taskTable.rows[i + 1] == null) {
         var row = taskTable.insertRow(i + 1);
@@ -20,22 +20,15 @@ function AgregarTarea(event) {
         var col1 = row.insertCell(0);
         var col2 = row.insertCell(1);
         var col3 = row.insertCell(2);
-        var col4 = row.insertCell(3);
 
         col1.innerHTML = i + 1;
         col2.innerHTML = taskText.value;
-        if (taskTime.value == "") {
-          col3.innerHTML = "-";
-        } else {
-          col3.innerHTML = taskTime.value;
-        }
 
         var checkBox = document.createElement("input");
         checkBox.type = "checkbox";
 
-        col4.appendChild(checkBox);
+        col3.appendChild(checkBox);
 
-        ActualizarTareas();
         VaciarInputs();
         break;
       }
@@ -53,7 +46,7 @@ function EliminarTareas() {
   // Recorro las filas de la tabla y compruebo si la celda numero 3 esta checkeada, si lo está se elimina la fila
 
   for (var i = 1; i <= taskTable.rows.length - 1; i++) {
-    if (taskTable.rows[i].cells[3].children[0].checked == true) {
+    if (taskTable.rows[i].cells[2].children[0].checked == true) {
       taskTable.deleteRow(i);
       exists = true;
       i = 0; // vuelvo el iterador a 0 para no saltar ninguna tarea
@@ -61,7 +54,6 @@ function EliminarTareas() {
   }
   // Si existe una tarea marcada se reorganizan los numero de la celda 0, si no, muestro una advertencia.
   exists ? ReasignarNumero() : window.alert("No hay tareas marcadas!");
-  ActualizarTareas();
 }
 
 function NoEsValido(taskText) {
@@ -89,34 +81,5 @@ function ReasignarNumero() {
 
   for (var i = 1; i <= taskTable.rows.length - 1; i++) {
     taskTable.rows[i].cells[0].innerHTML = i;
-  }
-}
-
-// Actualizo las tareas guardadas al guardar o eliminar tareas
-
-function ActualizarTareas() {
-  var data;
-
-  var taskTable = document.getElementById("taskTable");
-
-  for (var i = 1; i <= taskTable.rows.length - 1; i++) {
-    var obj = {
-      taskText: taskTable.rows[i].cells[1].innerHTML,
-      taskTime: taskTable.rows[i].cells[2].innerHTML,
-    };
-
-    if (data == null) {
-      data = [obj];
-    } else {
-      data.push(obj);
-    }
-  }
-
-  // Si no hay tareas para guardar borro el localStorage, si las hay actualizo en el localStorage
-  if (data == null) {
-    localStorage.removeItem("savedData");
-  } else {
-    localStorage.removeItem("savedData");
-    localStorage.setItem("savedData", JSON.stringify(data));
   }
 }
